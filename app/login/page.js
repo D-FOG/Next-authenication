d98bc60809049d59adfd30cb34265ad7f3a9cefe
unknown <ogbondafavour9@gmail.com>
2024-04-12 08:59:30 +0100

    updated login page and worked on the loginApi

--- app/login/page.js
+++ app/login/page.js
@@ -8,17 +8,52 @@ export default function Home() {
     email: '',
     password: '',
    });
+
+    const handleData = (e) => {
+        e.preventDefault();
+
+        const {name, value} = e.target;
+
+        setDetails(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    }   
+
+   const login = async(e) => {
+    e.preventDefault();
+
+    try {
+        const response = await fetch('/api/loginApi/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({loginDetail})
+        })
+
+        if (response.ok) {
+            const data = await response.json()
+            console.log('The response:', data)
+        } else {
+            const data = await response.json();
+            console.log(data.error);
+        }
+    } catch(error){
+        console.log(`An error occured ${error}`);
+    }
+   }
 console.log(loginDetail);
     return (
         <div className={styles.body}>
             <h1>Login</h1>
             <div className={styles.main}>
-                <form className={styles.signup}>
-                    <label for="email">Email: </label>
-                    <input type="email" placeholder="Enter email" id="email" onChange={(e) => setDetails({...loginDetail, email: e.target.value}) } required/>
+                <form className={styles.signup} onSubmit={login}>
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" placeholder="Enter email" id="email" name="email" onChange={handleData} required/>
                     <br/><br/>
-                    <label for="password">Password:</label>
-                    <input type="password" placeholder="Enter password" id="password" onChange={(e) => setDetails({...loginDetail,password: e.target.value })} required/>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" placeholder="Enter password" id="password" name="password" onChange={handleData} required/>
                     <br></br>
                     <button className={styles.button}>Submit</button>
                 </form>
--- pages/api/loginApi/login.js
+++ pages/api/loginApi/login.js
@@ -16,9 +16,20 @@ client
     .setProject(projectId)
 
 export default async function handler(req, res){
-    try{
+    if (req.method === "POST"){
+        const {loginDetail} = req.body;
+        const {email, password} = loginDetail;
+        console.log(loginDetail);
+        console.log(email, password);
 
-    }catch(error){
-        
+        try{
+            const response = account.createEmailSession(email, password);
+            //const email = loginDetails.email;
+            //const password = loginDetails.password;
+            console.log(response);
+            res.status(200).json({session: 'successful'});
+        }catch(error){
+            res.status(500).json({error: error});
+        }
     }
 }
